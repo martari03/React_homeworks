c441249d388b091423f1031a6d09620e9b1c3b82
martari03 <rishnyakmarta@gmail.com>
2023-02-17 15:53:48 +0200

    hw8

---
+++ app.ts
@@ -0,0 +1,22 @@
+//2) типізувати функції:
+
+import {IUser} from "./interfaces/userInterface";
+
+const user: IUser = {name: "Max", age: 18, gender: 'male'}
+
+function sum(a: number, b: number): number {
+    return a + b;
+}
+
+function showSum(a: number, b: number): void {
+    console.log(a + b);
+}
+
+function incAge(someUser: IUser, inc: number): IUser {
+    someUser.age += inc;
+    return someUser;
+}
+
+console.log(sum(1, 2));
+showSum(2, 3);
+incAge(user, 2);
